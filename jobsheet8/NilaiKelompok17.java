16415aba834d6da8b6968c11da51af72cffc7cf5
Muflihmalikal <127600522+Muflihmalikal@users.noreply.github.com>
2025-11-14 01:08:37 +0700

    Kelompok update

---
+++ jobsheet8/NilaiKelompok17.java
@@ -0,0 +1,33 @@
+package jobsheet8;
+
+import java.util.Scanner;
+
+public class NilaiKelompok17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i, j, nilai, kelompokTertinggi = 0;
+        float totalNilai, rataNilai, rataTertinggi = 0;
+        i = 1;
+        while (i <= 6) {
+            System.out.println("Kelompok " + i);
+            totalNilai = 0;
+            for (j = 1; j <= 5; j++) {
+                System.out.print("Nilai dari kelompok Penilai " + j + ": ");
+                nilai = sc.nextInt();
+                totalNilai += nilai;
+            }
+            rataNilai = totalNilai / 5;
+            System.out.println("Kelompok " + i + ": nilai rata - rata = " + rataNilai);
+            if (rataNilai > rataTertinggi) {
+                rataTertinggi = rataNilai;
+                kelompokTertinggi = i;
+            }
+            i++;
+        }
+        System.out.println("\n==========================================");
+        System.out.println("Kelompok dengan rata-rata tertinggi adalah:");
+        System.out.println("Kelompok " + kelompokTertinggi + " dengan rata-rata " + rataTertinggi);
+        System.out.println("==========================================");
+        sc.close();
+    }
+}

6b839b585008462c0bfffd1664e72b75b2e5c3d4
Muflihmalikal <127600522+Muflihmalikal@users.noreply.github.com>
2025-11-14 00:34:31 +0700

    bintang looping

---
+++ jobsheet8/Star17.java
@@ -0,0 +1,16 @@
+package jobsheet8;
+
+import java.util.Scanner;
+
+public class Star17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan nilai n = ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i <= n; i += 2) {
+            System.out.print("*");
+        }
+        sc.close();
+    }
+}

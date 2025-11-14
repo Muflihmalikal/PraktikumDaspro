c4a886ce191ee9f218e7f587d57a4a3cdcd9a7d8
Muflihmalikal <127600522+Muflihmalikal@users.noreply.github.com>
2025-11-14 07:11:53 +0700

    Tugas 1

---
+++ jobsheet8/tugas1_17.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class tugas1_17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("=====Perhitungan Kuadrat=====");
+        System.out.print("Masukkan Nilai N = ");
+        int n = sc.nextInt();
+        int total = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int kuadrat = i * i;
+            total += kuadrat;
+
+            System.out.print("n = " + i + " jumlah kuadrat = ");
+            
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j * j);
+                if (j < i)
+                    System.out.print(" + ");
+            }
+
+            System.out.println(" = " + total);
+        }
+
+    }
+}

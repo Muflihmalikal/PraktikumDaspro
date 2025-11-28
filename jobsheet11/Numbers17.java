6c52bc4feea78361ac8fa037ac92530c9e073a36
Muflihmalikal <127600522+Muflihmalikal@users.noreply.github.com>
2025-11-28 15:21:07 +0700

    soal

--- jobsheet11/Numbers17.java
+++ jobsheet11/Numbers17.java
@@ -15,5 +15,8 @@ public class Numbers17 {
         for (int i = 0; i < myNumbers.length; i++) {
             System.out.println(Arrays.toString(myNumbers[i]));
         }
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println("Panjang baris ke-" + (i + 1) + ": " + myNumbers[i].length);
+        }
     }
 }

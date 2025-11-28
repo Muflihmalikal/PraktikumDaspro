6aba43175d091dcd83c427eb5696e37f04962c65
Muflihmalikal <127600522+Muflihmalikal@users.noreply.github.com>
2025-11-28 14:33:53 +0700

    pertanyaan 1

--- jobsheet11/Numbers17.java
+++ jobsheet11/Numbers17.java
@@ -1,5 +1,7 @@
 package jobsheet11;
 
+import java.util.Arrays;
+
 public class Numbers17 {
     public static void main(String[] args) {
         String[][] myNumbers = new String[3][];
@@ -9,6 +11,9 @@ public class Numbers17 {
         myNumbers[0][1] = "Muflih";
         myNumbers[0][3] = "Dewi";
         myNumbers[0][4] = "Anin";
-
+        // System.out.prStringln(myNumbers[0]);
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
     }
 }

6342acac2340214d9daf16c2b7cc76c1385c11c5
Muflihmalikal <127600522+Muflihmalikal@users.noreply.github.com>
2025-11-28 15:36:57 +0700

    modif pertanyaan 1

--- jobsheet11/SIAKAD17.java
+++ jobsheet11/SIAKAD17.java
@@ -5,7 +5,11 @@ import java.util.Scanner;
 public class SIAKAD17 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int nilai[][] = new int[4][3];
+        System.out.println("Masukkan Jumlah Siswa : ");
+        int siswa = sc.nextInt();
+        System.out.println("Masukkan Jumlah Siswa : ");
+        int matkul = sc.nextInt();
+        int nilai[][] = new int[siswa][matkul];
         for (int i = 0; i < nilai.length; i++) {
             System.out.println("Input nilai mahasiswa ke-" + (i + 1));
             double totalPersiswa = 0;
@@ -14,7 +18,16 @@ public class SIAKAD17 {
                 nilai[i][j] = sc.nextInt();
                 totalPersiswa += nilai[i][j];
             }
-            System.out.println("Nilai Rata-rata: " + totalPersiswa / 3);
+            System.out.println("Nilai Rata-rata: " + totalPersiswa / nilai.length);
+        }
+        System.out.println("\n=================");
+        System.out.println("Rata-rata Nilai setiap Mata Kuliah: ");
+        for (int j = 0; j < nilai[0].length; j++) {
+            double totalPermatkul = 0;
+            for (int i = 0; i < nilai.length; i++) {
+                totalPermatkul += nilai[i][j];
+            }
+            System.out.println("Mata kuliah" + (j + 1) + ": " + totalPermatkul / nilai[0].length);
         }
         sc.close();
     }

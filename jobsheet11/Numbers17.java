9eae7500393c896598335c1fcfd0ae27427da15f
Muflihmalikal <127600522+Muflihmalikal@users.noreply.github.com>
2025-11-28 14:33:36 +0700

    joobseheet 11

---
+++ jobsheet11/Numbers17.java
@@ -0,0 +1,14 @@
+package jobsheet11;
+
+public class Numbers17 {
+    public static void main(String[] args) {
+        String[][] myNumbers = new String[3][];
+        myNumbers[0] = new String[5];
+        myNumbers[1] = new String[3];
+        myNumbers[2] = new String[1];
+        myNumbers[0][1] = "Muflih";
+        myNumbers[0][3] = "Dewi";
+        myNumbers[0][4] = "Anin";
+
+    }
+}

5460d60794de835f5da0423405bc9028caf96084
Muflihmalikal <127600522+Muflihmalikal@users.noreply.github.com>
2025-11-28 15:29:28 +0700

    modif soal 7

--- jobsheet11/SIAKAD17.java
+++ jobsheet11/SIAKAD17.java
@@ -8,10 +8,13 @@ public class SIAKAD17 {
         int nilai[][] = new int[4][3];
         for (int i = 0; i < nilai.length; i++) {
             System.out.println("Input nilai mahasiswa ke-" + (i + 1));
+            double totalPersiswa = 0;
             for (int j = 0; j < nilai[i].length; j++) {
                 System.out.print("Nilai mata kuliah" + (j + 1) + ": ");
                 nilai[i][j] = sc.nextInt();
+                totalPersiswa += nilai[i][j];
             }
+            System.out.println("Nilai Rata-rata: " + totalPersiswa / 3);
         }
         sc.close();
     }

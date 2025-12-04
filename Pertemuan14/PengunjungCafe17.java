9124f66fbb9c1436111d861de3e9f0f5563e009e
Muflihmalikal <127600522+Muflihmalikal@users.noreply.github.com>
2025-12-04 20:01:12 +0700

    Percobaan 4

---
+++ Pertemuan14/PengunjungCafe17.java
@@ -0,0 +1,14 @@
+package Pertemuan14;
+
+public class PengunjungCafe17 {
+    static void daftarPengunjung(String... namaPengunjung) {
+        System.out.println("Daftar Nama Pengunjung");
+        for (int i = 0; i < namaPengunjung.length; i++) {
+            System.out.println("- " + namaPengunjung[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        daftarPengunjung("Ali","Budi","Citra");
+    }
+}

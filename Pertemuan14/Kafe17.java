d9b72f450dddd7aec09e2f5ddf3b61306ce47606
Muflihmalikal <127600522+Muflihmalikal@users.noreply.github.com>
2025-12-04 19:21:41 +0700

    Percobaan 3

--- Pertemuan14/Kafe17.java
+++ Pertemuan14/Kafe17.java
@@ -1,7 +1,10 @@
 package Pertemuan14;
 
+import java.util.Scanner;
+
 public class Kafe17 {
     public static void menu(String namaPelanggan, boolean isMember, String kodePromo) {
+
         System.out.println("Selamat Datang, " + namaPelanggan + "!");
         if (isMember) {
             System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian");
@@ -24,7 +27,20 @@ public class Kafe17 {
         System.out.println("Silahkan pilih menu yang anda inginkan");
     }
 
+    public static int hitungTotalHarga17(int pilihanMenu, int banyakItem) {
+        int[] hargaItems = { 15000, 20000, 22000, 12000, 10000, 18000 };
+        int hargaTotal = hargaItems[pilihanMenu - 1] * banyakItem;
+        return hargaTotal;
+    }
+
     public static void main(String[] args) {
-        menu("Budi", true, "DISKON30");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\nMasukkan nomor menu yang ingin anda pesan: ");
+        int pilihanMenu = sc.nextInt();
+        System.out.println("Masukkan jumlah item yang ingin dipean: ");
+        int banyakItem = sc.nextInt();
+        int totalHarga = hitungTotalHarga17(pilihanMenu, banyakItem);
+        System.out.println("Total harga untuk pemesanan anda: Rp" + totalHarga);
+        sc.close();
     }
 }

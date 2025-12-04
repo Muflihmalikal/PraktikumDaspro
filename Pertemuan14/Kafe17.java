e2eb04712ab46ea01268b2dbae68b4d61aad2a98
Muflihmalikal <127600522+Muflihmalikal@users.noreply.github.com>
2025-12-04 19:34:58 +0700

    Modifikasi Percobaan 3 soal 3

--- Pertemuan14/Kafe17.java
+++ Pertemuan14/Kafe17.java
@@ -27,10 +27,18 @@ public class Kafe17 {
         System.out.println("Silahkan pilih menu yang anda inginkan");
     }
 
-    public static int hitungTotalHarga17(int pilihanMenu, int banyakItem) {
+    public static int hitungTotalHarga17(int pilihanMenu, int banyakItem, String kodePromo) {
         int[] hargaItems = { 15000, 20000, 22000, 12000, 10000, 18000 };
         int hargaTotal = hargaItems[pilihanMenu - 1] * banyakItem;
-        return hargaTotal;
+        int diskon = 0;
+        if (kodePromo.equalsIgnoreCase("DISKON50")) {
+            diskon = hargaTotal * 50 / 100;
+        } else if (kodePromo.equalsIgnoreCase("DISKON30")) {
+            diskon = hargaTotal * 30 / 100;
+        } else {
+            System.out.println("Kode Invalid");
+        }
+        return hargaTotal - diskon;
     }
 
     public static void main(String[] args) {
@@ -39,7 +47,9 @@ public class Kafe17 {
         int pilihanMenu = sc.nextInt();
         System.out.println("Masukkan jumlah item yang ingin dipean: ");
         int banyakItem = sc.nextInt();
-        int totalHarga = hitungTotalHarga17(pilihanMenu, banyakItem);
+        System.out.println("Masukkan Kode Promo: ");
+        String kodePromo = sc.nextLine();
+        int totalHarga = hitungTotalHarga17(pilihanMenu, banyakItem, kodePromo);
         System.out.println("Total harga untuk pemesanan anda: Rp" + totalHarga);
         sc.close();
     }
